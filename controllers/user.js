52b5664fa484b58ec16e1f6be4de6cf2d93a8c82
matisantillan11 <matias1.santillan@gmail.com>
2021-10-13 21:05:36 -0300

    adding password hash and validations of body to POST method

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,30 @@
 const { request, response } = require('express')
-
+const bcrypt = require('bcrypt')
 const firebase = require('firebase-admin')
 
 const postUser = async (req = request, res = response) => {
-	const userRef = firebase.database().ref().child('users')
-	const { fullname, dni, role, email } = req.body
-	const user = { fullname, dni, role, email }
+	try {
+		const userRef = firebase.database().ref().child('users')
+		const { fullname, dni, role, email, password, school } = req.body
+		const salt = 10
+		const hashedPassword = await bcrypt.hash(password, salt)
 
-	await userRef.push({ user })
+		const user = {
+			fullname,
+			dni,
+			role,
+			email,
+			school,
+			password: hashedPassword,
+		}
 
-	res.status(200).send(user)
+		await userRef.push(user)
+		delete user.password
+
+		res.status(200).send(user)
+	} catch (error) {
+		res.status(500).json(error.message)
+	}
 }
 
 module.exports = { postUser }
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const { Router, response, request } = require('express')
+const { check } = require('express-validator')
 const { postUser } = require('../controllers/user')
+const validateEntries = require('../middlewares/validateEntries')
 
 const router = Router()
 
@@ -10,7 +12,24 @@ router.get('/', (req = request, res = response) => {
 	})
 })
 
-router.post('/', postUser)
+router.post(
+	'/',
+	[
+		check('fullname', 'You need to provide your fullname').isString(),
+		check('dni', 'You need to provide a valid DNI')
+			.isNumeric()
+			.isLength({ min: 7, max: 10 }),
+		check('email', 'You need to provide your email').isEmail(),
+		check('password', 'You must to provide a password').isString().notEmpty(),
+		check(
+			'password',
+			'You password must contain between 8 and 12 characters',
+		).isLength({ min: 8, max: 12 }),
+		check('school').default('Instituto Carlos Pellegrini'),
+		validateEntries,
+	],
+	postUser,
+)
 
 router.put('/', (req = request, res = response) => {
 	res.json({
